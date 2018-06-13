5cf2b02ee3c9cb9114540da4ea69f8fe211b5023
canro91 <canro91@hotmail.com>
2018-06-13 18:04:34 -0500

    Add Extensions

---
+++ Parsinator/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Parsinator
+{
+    public static class DictionaryExtensions
+    {
+        // HACK: Details are returned as <KeyName[index], <String, String>>
+        public static DataSet ToDataSet(this Dictionary<string, Dictionary<string, string>> dict, DataSet dataSet)
+        {
+            foreach (var item in dict.Where(t => !t.Key.Contains('[')))
+            {
+                var sectionName = item.Key;
+                var parsed = item.Value;
+
+                if (!dataSet.Tables.Contains(sectionName))
+                    throw new ArgumentNullException($"DataSet [{dataSet.DataSetName}] doesn't contain a table: [{sectionName}]");
+
+                DataRow row = dataSet.Tables[sectionName].NewRow();
+                foreach (var p in parsed)
+                {
+                    row[p.Key] = p.Value;
+                }
+                dataSet.Tables[sectionName].Rows.Add(row);
+            }
+
+            var keys = dict.Where(t => t.Key.Contains('['))
+                           .Select(t => t.Key.Substring(0, t.Key.IndexOf('[')))
+                           .Distinct();
+
+            foreach (var item in dict.Where(t => t.Key.Contains('[')))
+            {
+                var sectionName = keys.FirstOrDefault(t => item.Key.StartsWith(t));
+                var parsed = item.Value;
+
+                DataRow row = dataSet.Tables[sectionName].NewRow();
+                foreach (var p in parsed)
+                {
+                    row[p.Key] = p.Value;
+                }
+                dataSet.Tables[sectionName].Rows.Add(row);
+            }
+
+            return dataSet;
+        }
+    }
+}
---
+++ Parsinator/Extensions/ISkipExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Parsinator
+{
+    public static class ISkipExtensions
+    {
+        public static List<List<String>> Chain(this IList<ISkip> skips, List<List<String>> lines)
+        {
+            List<List<String>> pages = lines;
+            if (skips != null && skips.Any())
+            {
+                foreach (var s in skips)
+                {
+                    pages = s.Skip(pages);
+                }
+            }
+            return pages;
+        }
+    }
+}
