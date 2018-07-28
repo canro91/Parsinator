88276fc7bd74d6b5ba9204a74aa9989c3f5ecd64
canro91 <canro91@hotmail.com>
2018-07-28 08:51:47 -0500

    Add SkipLineCountFromEnd

---
+++ Parsinator/Skippers/SkipLineCountFromEnd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Parsinator
+{
+    public class SkipLineCountFromEnd : ISkip
+    {
+        private readonly int LineCount;
+
+        public SkipLineCountFromEnd(int lineCount = 1)
+        {
+            this.LineCount = lineCount;
+        }
+
+        public List<List<string>> Skip(List<List<string>> lines)
+        {
+            var skipped = lines.Select(l => l.Take(l.Count - LineCount).ToList())
+                               .ToList();
+            return skipped;
+        }
+    }
+}
