d6f8320e2b8097f56409c06d48a3c1991ed3fa6d
canro91 <canro91@hotmail.com>
2018-08-16 22:08:19 -0500

    Parse GPS frame with multiple group regex
    It parses a sample GPS frame using multiple group regex and parsing
    from the encoded body of the frame. IMEI values were replaced by a dummy
    value. See http://aprs.gids.nl/nmea/

--- Parsinator.Sample/FrameTests.cs
+++ Parsinator.Sample/FrameTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Parsinator.Sample
 {
@@ -41,6 +42,73 @@ namespace Parsinator.Sample
             Assert.AreEqual("3131333035392E30302C412C313032342E37363537392C4E2C30373532382E30313237392C572C302E3030302C2C3235303131377C31322E327C3139347C303030307C303030302C303030307C303238303330343339", ds["Key"]["Body"]);
             Assert.AreEqual("5BF8", ds["Key"]["CheckSum"]);
             Assert.AreEqual("0D0A", ds["Key"]["EOF"]);
+        }
+
+        [Test]
+        public void Parse_Frame_ParsesBody()
+        {
+            var frame = "242400676315802812345699553131333035392E30302C412C313032342E37363537392C4E2C30373532382E30313237392C572C302E3030302C2C3235303131377C31322E327C3139347C303030307C303030302C303030307C3032383033303433395BF80D0A";
+
+            var p = new Dictionary<String, IList<IParse>>
+            {
+                {
+                    "Key",
+                    new List<IParse>
+                    {
+                        new FromLineWithCountAfterPosition(key: "Header",  lineNumber: 1, startPosition: 0, charCount: 4),
+                        new FromLineWithCountAfterPosition(key: "Length",  lineNumber: 1, startPosition: 4, charCount: 4),
+                        new FromLineWithCountAfterPosition(key: "IMEI",    lineNumber: 1, startPosition: 8, charCount: 14),
+                        new FromLineWithCountAfterPosition(key: "Cmd",     lineNumber: 1, startPosition: 22, charCount: 4),
+                        new FromOutput(
+                            parseFrom: new FromLineWithCountAfterPosition(key: "Body", lineNumber: 1, startPosition: 26, charCount: -8, factory: (str) => ConvertHexToAscii(str)),
+                            parsers: new List<IParse>
+                            {
+                                new FromMultiGroupRegex(pattern: new Regex(@"(?<TimeStamp>\d{6}\.\d{2}),(?<Validity>A|V),(?<Latitude>\d{4,5}\.\d{5}),(?<NorthSouth>N|S),(?<Longitude>\d{4,5}\.\d{5}),(?<EastWest>E|W),(?<SpeedInKnots>\d+\.\d+),(?<TrueCourse>\d+\.\d+)?,(?<DateStamp>\d{6})\|(?<Custom>\S*)"))
+                            }),
+                        new FromLineWithCountAfterPosition(key: "CheckSum",lineNumber: 1, startPosition: -8, charCount: 4),
+                        new FromLineWithCountAfterPosition(key: "EOF",     lineNumber: 1, startPosition: -4, charCount: 4),
+                    }
+                }
+            };
+
+            var lines = new List<List<string>> { new List<string> { frame } };
+
+            var parser = new Parser(p);
+            var ds = parser.Parse(lines);
+
+            Assert.AreEqual("2424", ds["Key"]["Header"]);
+            Assert.AreEqual("0067", ds["Key"]["Length"]);
+            Assert.AreEqual("63158028123456", ds["Key"]["IMEI"]);
+            Assert.AreEqual("9955", ds["Key"]["Cmd"]);
+
+            Assert.AreEqual("113059.00", ds["Key"]["TimeStamp"]);
+            Assert.AreEqual("A", ds["Key"]["Validity"]);
+            Assert.AreEqual("1024.76579", ds["Key"]["Latitude"]);
+            Assert.AreEqual("N", ds["Key"]["NorthSouth"]);
+            Assert.AreEqual("07528.01279", ds["Key"]["Longitude"]);
+            Assert.AreEqual("W", ds["Key"]["EastWest"]);
+            Assert.AreEqual("0.000", ds["Key"]["SpeedInKnots"]);
+            Assert.AreEqual("", ds["Key"]["TrueCourse"]);
+            Assert.AreEqual("250117", ds["Key"]["DateStamp"]);
+            Assert.AreEqual("12.2|194|0000|0000,0000|028030439", ds["Key"]["Custom"]);
+
+            Assert.AreEqual("5BF8", ds["Key"]["CheckSum"]);
+            Assert.AreEqual("0D0A", ds["Key"]["EOF"]);
+        }
+
+        private string ConvertHexToAscii(String hexString)
+        {
+            string ascii = string.Empty;
+
+            for (int i = 0; i < hexString.Length; i += 2)
+            {
+                String hs = hexString.Substring(i, 2);
+                uint decval = System.Convert.ToUInt32(hs, 16);
+                char character = System.Convert.ToChar(decval);
+                ascii += character;
+            }
+
+            return ascii;
         }
     }
 }
