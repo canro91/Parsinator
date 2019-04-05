e2165c97386dd65aacd641d8c1151275f3fe1f19
canro91 <canro91@hotmail.com>
2019-04-05 17:47:37 -0500

    Add test to show how to support nested xml nodes

--- Parsinator.Tests/ParseTests.DataSet.cs
+++ Parsinator.Tests/ParseTests.DataSet.cs
@@ -71,6 +71,68 @@ Street name: Main; City: Wonderland");
         }
 
         [Test]
+        public void Parse_MultipleParsers_BuildsXmlWithNestedNodes()
+        {
+            var ds = new DataSet("Person")
+                        .WithTable(new DataTable("FullName")
+                            .WithColumn("Name")
+                            .WithColumn("LastName"))
+                        .WithTable(new DataTable("Address")
+                            .WithColumn("StreetName")
+                            .WithColumn("City"))
+                        .WithTable(new DataTable("Country")
+                            .WithColumn("State")
+                            .WithColumn("Country"))
+                        .WithRelation("FullName", "Address")
+                        .WithRelation("Address", "Country");
+
+            var p = new Dictionary<String, IList<IParse>>
+            {
+                {
+                    "FullName",
+                    new List<IParse>
+                    {
+                        new ParseFromLineNumberWithRegex(key: "Name", lineNumber: 1, pattern: new Regex(@"Name:\s*(\w+);")),
+                        new ParseFromLineNumberWithRegex(key: "LastName", lineNumber: 1, pattern: new Regex(@"Last name:\s*(\w+)")),
+                    }
+                },
+                {
+                    "Address",
+                    new List<IParse>
+                    {
+                        new ParseFromLineNumberWithRegex(key: "StreetName", lineNumber: 2, pattern: new Regex(@"Street name:\s*(\w+);")),
+                        new ParseFromLineNumberWithRegex(key: "City", lineNumber: 2, pattern: new Regex(@"City:\s*(\w+)")),
+                    }
+                },
+                {
+                    "Country",
+                    new List<IParse>
+                    {
+                        new ParseFromLineNumberWithRegex(key: "State", lineNumber: 3, pattern: new Regex(@"State:\s*(\w+);")),
+                        new ParseFromLineNumberWithRegex(key: "Country", lineNumber: 3, pattern: new Regex(@"Country:\s*(\w+)")),
+                    }
+                }
+            };
+            var lines = FromText(@"
+Name: John; Last name: Doe
+Street name: Main; City: Wonderland
+State: Somewhere; Country: United States of Wonderland");
+
+            var parser = new Parser(p);
+            var parsed = parser.Parse(lines);
+
+            var xml = parsed.ToDataSet(ds).GetXml();
+
+            Assert.AreEqual(@"<Person>
+  <FullName Name=""John"" LastName=""Doe"">
+    <Address StreetName=""Main"" City=""Wonderland"">
+      <Country State=""Somewhere"" Country=""United"" />
+    </Address>
+  </FullName>
+</Person>", xml);
+        }
+
+        [Test]
         public void Parse_NotExistingTable_BuildsXml()
         {
             var ds = new DataSet("DataSetName")
