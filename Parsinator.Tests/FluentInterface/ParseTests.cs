6e159273511bb0cc68bc035c75f285e5f1115702
canro91 <canro91@hotmail.com>
2020-08-12 17:49:14 -0500

    Create builders for parsers in a given lines and regex

--- Parsinator.Tests/FluentInterface/ParseTests.cs
+++ Parsinator.Tests/FluentInterface/ParseTests.cs
@@ -34,7 +34,7 @@ Value: 123456");
         }
 
         [Test]
-        public void Parse_AnyInput_ReturnGivenValue()
+        public void Parse_AnyInput_ReturnsGivenValue()
         {
             var p = new Dictionary<String, IList<IParse>>
             {
@@ -53,6 +53,81 @@ Value: 123456");
             var ds = parser.Parse(lines);
 
             Assert.AreEqual("Another value", ds["Key"]["Value"]);
+        }
+
+        [Test]
+        public void Parse_MultipleLineStringUntilRegex_ParsesStringFromLineNumberUntilRegex()
+        {
+            var p = new Dictionary<String, IList<IParse>>
+            {
+                {
+                    "Key",
+                    new List<IParse>
+                    {
+                        Parse.Key("Value").FromLine(1).UntilRegex(new Regex(@"--End--")),
+                    }
+                }
+            };
+            var lines = FromText(@"
+1 Any
+2 value
+--End--");
+
+            var parser = new Parser(p);
+            var ds = parser.Parse(lines);
+
+            Assert.AreEqual("1 Any 2 value", ds["Key"]["Value"]);
+        }
+
+        [Test]
+        public void Parse_PatternAndLineNumber_ParsesRegexInTheLine()
+        {
+            var p = new Dictionary<String, IList<IParse>>
+            {
+                {
+                    "Key",
+                    new List<IParse>
+                    {
+                        Parse.Key("Value").FromLine(2).Regex(new Regex(@"Value:\s*(\d+)"))
+                    }
+                }
+            };
+            var lines = FromText(@"
+1 This line will be ignored   -3
+2 Value: 123456               -2
+3 This line will be ignored   -1");
+
+            var parser = new Parser(p);
+            var ds = parser.Parse(lines);
+
+            Assert.AreEqual("123456", ds["Key"]["Value"]);
+        }
+
+        [Test]
+        public void Parse_MultipleLineStringBetweenTwoRegexes_ParseStringBetweenRegexes()
+        {
+            var p = new Dictionary<String, IList<IParse>>
+            {
+                {
+                    "Key",
+                    new List<IParse>
+                    {
+                        Parse.Key("Value").FromRegex(new Regex(@"--Begin--")).ToRegex(new Regex(@"--End--")),
+                    }
+                }
+            };
+            var lines = FromText(@"
+This line will be ignored
+--Begin--
+Any
+value
+--End--
+This line will be ignored");
+
+            var parser = new Parser(p);
+            var ds = parser.Parse(lines);
+
+            Assert.AreEqual("Any value", ds["Key"]["Value"]);
         }
 
         private List<List<String>> FromText(String str)
--- Parsinator/FluentInterface/Parse.cs
+++ Parsinator/FluentInterface/Parse.cs
@@ -21,7 +21,50 @@ namespace Parsinator.FluentInterface
         public ParseFromRegex Regex(Regex regex)
             => new ParseFromRegex(Key, regex);
 
-        public IParse Value(string value)
+        public ParseFromValue Value(string value)
             => new ParseFromValue(Key, value);
+
+        public FromLineBuilder FromLine(int lineNumber)
+        {
+            var builder = new FromLineBuilder
+            {
+                Key = Key,
+                LineNumber = lineNumber
+            };
+            return builder;
+        }
+
+        public FromRegexBuilder FromRegex(Regex regex)
+        {
+            var builder = new FromRegexBuilder
+            {
+                Key = Key,
+                First = regex
+            };
+            return builder;
+        }
+    }
+
+    public class FromLineBuilder
+    {
+        public string Key { get; internal set; }
+
+        public int LineNumber { get; internal set; }
+
+        public ParseFromLineNumberUntilFirstMatchOfRegex UntilRegex(Regex regex)
+            => new ParseFromLineNumberUntilFirstMatchOfRegex(Key, LineNumber, regex);
+
+        public ParseFromLineNumberWithRegex Regex(Regex regex)
+            => new ParseFromLineNumberWithRegex(Key, LineNumber, regex);
+    }
+
+    public class FromRegexBuilder
+    {
+        public string Key { get; set; }
+
+        public Regex First { get; set; }
+
+        public ParseFromRegexToRegex ToRegex(Regex regex)
+            => new ParseFromRegexToRegex(Key, First, regex);
     }
 }
